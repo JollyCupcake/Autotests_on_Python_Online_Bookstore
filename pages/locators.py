13009890accefa89beedb6036e9f327f277bdf88
JollyCupcake <60314990+JollyCupcake@users.noreply.github.com>
2024-03-29 18:39:23 +0400

    added locators for the new page (product_page)

--- pages/locators.py
+++ pages/locators.py
@@ -2,8 +2,15 @@ from selenium.webdriver.common.by import By
 
 
 class MainPageLocators():
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link") # Селектор для ссылки перехода на форму логина
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link") # Ссылка перехода на форму логина
 
 class LoginPageLocators():
-    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form") # Селектор для формы логина
-    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form") # Селектор для формы регистрации
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form") # Форма логина
+    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form") # Форма регистрации
+
+class ProductPageLocators():
+    ADD_TO_BASKET_BUTTON = (By.CLASS_NAME, "btn-add-to-basket") # Кнопка "Добавить в корзину"
+    PRODUCT_NAME = (By.CSS_SELECTOR, "[class='col-sm-6 product_main'] h1") # Наименованин продукта
+    PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main p[class='price_color']") # Стоимость продукта
+    ADDED_PRODUCT_NAME = (By.CSS_SELECTOR, "#messages div:nth-child(1) strong") # Наименование добавленного в корзину продукта
+    ADDED_PRODUCT_PRICE = (By.CSS_SELECTOR, "[class='alert alert-safe alert-noicon alert-info  fade in'] strong") # Цена добавленного в корзину продукта
