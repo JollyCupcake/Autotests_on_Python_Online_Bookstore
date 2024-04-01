f451068b3bfa52e66b6ba7d75f92db2ef33e3da2
JollyCupcake <60314990+JollyCupcake@users.noreply.github.com>
2024-04-01 15:59:05 +0400

    Added parametrization for testing several links, one link marked as xfailed to prevent complete failing until the bug is fixed

--- test_product_page.py
+++ test_product_page.py
@@ -4,9 +4,21 @@ from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 from .pages.product_page import ProductPage
 import time
+import pytest
 
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+@pytest.mark.parametrize('promocode', ["?promo=offer0",
+                                       "?promo=offer1",
+                                       "?promo=offer2",
+                                       "?promo=offer3",
+                                       "?promo=offer4",
+                                       "?promo=offer5",
+                                       "?promo=offer6",
+                                       pytest.param("?promo=offer7", marks=pytest.mark.xfail), 
+                                       "?promo=offer8",
+                                       "?promo=offer9"])
+                                       
+def test_guest_can_add_product_to_basket(browser, promocode):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/{promocode}"
     product_page = ProductPage(browser, link) # инициализируем Page Object ProductPage
     product_page.open()                       
     product_page.add_product_to_basket()  # вызываем метод добавления товара в корзину
