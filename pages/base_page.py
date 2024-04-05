1facae91e0347f4a70325443f592cabd23d45891
JollyCupcake <60314990+JollyCupcake@users.noreply.github.com>
2024-04-05 15:12:55 +0400

    added new a new method for going to the basket page

--- pages/base_page.py
+++ pages/base_page.py
@@ -5,6 +5,7 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from .locators import BasePageLocators
+from .locators import MainPageLocators
 
 class BasePage():
     def __init__(self, browser, url, timeout=10):
@@ -17,6 +18,10 @@ class BasePage():
 
     def go_to_login_page(self):
         link = self.browser.find_element(*BasePageLocators.LOGIN_LINK) #для данной проверки можно использовать невалидный локатор LOGIN_LINK_INVALID 
+        link.click()
+
+    def go_to_basket_page(self):
+        link = self.browser.find_element(*MainPageLocators.BASKET_LINK)
         link.click()
 
     def is_element_present(self, how, what):
