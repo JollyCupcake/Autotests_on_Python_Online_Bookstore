b7262e2b2d4eb6ff8f98456a0dd1f3a6b0fd4d3d
JollyCupcake <60314990+JollyCupcake@users.noreply.github.com>
2024-04-05 15:15:47 +0400

    Added a new test to check if the basket is empty from the main page

--- test_main_page.py
+++ test_main_page.py
@@ -2,6 +2,9 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
+from .pages.basket_page import BasketPage
+from .pages.product_page import ProductPage
+import pytest
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
@@ -11,10 +14,17 @@ def test_guest_can_go_to_login_page(browser):
     login_page = LoginPage(browser, browser.current_url) # инициализируем Page Object LoginPage
     login_page.should_be_login_page()  # вызываем метод - проверку наличия форм регистрации и логина, проверку урла
 
-
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
 
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_basket_page() #переход на страницу корзины
+    basket_page = BasketPage(browser, link)
+    basket_page.should_be_message_that_basket_empty()
+    basket_page.basket_is_empty()
