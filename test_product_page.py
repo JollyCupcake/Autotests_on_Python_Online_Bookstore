8e0843c104b12bf1e49c81451990d78b487a0570
JollyCupcake <60314990+JollyCupcake@users.noreply.github.com>
2024-05-06 17:45:36 +0400

    1. added back a test to check that guest can add products to a basket; 2. removed odd comments

--- test_product_page.py
+++ test_product_page.py
@@ -21,39 +21,49 @@ class TestUserAddToBasketFromProductPage():
     
     def test_user_cant_see_success_message(self, browser):
         link = "https://selenium1py.pythonanywhere.com/en-gb/catalogue/hacking-exposed-wireless_208/"
-        product_page = ProductPage(browser, link) # инициализируем Page Object ProductPage
-        product_page.open()                       
-        product_page.should_not_be_success_message_element_not_present() 
+        product_page = ProductPage(browser, link)
+        product_page.open()
+        product_page.should_not_be_success_message_element_not_present()
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         link = "https://selenium1py.pythonanywhere.com/en-gb/catalogue/hacking-exposed-wireless_208/"
-        product_page = ProductPage(browser, link) # инициализируем Page Object ProductPage
-        product_page.open()                       
-        product_page.add_product_to_basket()  # вызываем метод добавления товара в корзину
-        # product_page.solve_quiz_and_get_code() #вызываем метод расчёта лоя страниц с промо
+        product_page = ProductPage(browser, link)
+        product_page.open()
+        product_page.add_product_to_basket()
         time.sleep(5)
         assert product_page.get_product_name() == product_page.get_added_product_name(), "Product added name is not equal to the product original name"
         assert product_page.get_product_price() == product_page.get_added_product_price(), "Basket price is not equal to the product original price"
 
 
+@pytest.mark.need_review
+def test_guest_can_add_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    product_page = ProductPage(browser, link)
+    product_page.open()
+    product_page.add_product_to_basket()
+    product_page.solve_quiz_and_get_code()
+    time.sleep(3)
+    assert product_page.get_product_name() == product_page.get_added_product_name(), "Product added name is not equal to the product original name"
+    assert product_page.get_product_price() == product_page.get_added_product_price(), "Basket price is not equal to the product original price"
+
 @pytest.mark.xfail
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     link = "https://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-    product_page = ProductPage(browser, link) # инициализируем Page Object ProductPage
-    product_page.open()                       
-    product_page.add_product_to_basket()  # вызываем метод добавления товара в корзину
-    product_page.solve_quiz_and_get_code() #вызываем метод расчёта
-    # time.sleep(5)
-    product_page.should_not_be_success_message_element_not_present() 
+    product_page = ProductPage(browser, link)
+    product_page.open()
+    product_page.add_product_to_basket()
+    product_page.solve_quiz_and_get_code()
+    time.sleep(3)
+    product_page.should_not_be_success_message_element_not_present()
     
 @pytest.mark.xfail
 def test_message_disappeared_after_adding_product_to_basket(browser):
     link = "https://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-    product_page = ProductPage(browser, link) # инициализируем Page Object ProductPage
-    product_page.open()                       
-    product_page.add_product_to_basket()  # вызываем метод добавления товара в корзину
-    product_page.solve_quiz_and_get_code() #вызываем метод расчёта
-    # time.sleep(1)
+    product_page = ProductPage(browser, link)
+    product_page.open()  
+    product_page.add_product_to_basket()
+    product_page.solve_quiz_and_get_code()
     product_page.should_not_be_success_message_element_disappeared()
 
 def test_guest_should_see_login_link_on_product_page(browser):
@@ -62,17 +72,19 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.open()
     page.should_be_login_link()
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
 
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "https://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/"
     page = MainPage(browser, link)
     page.open()
-    page.go_to_basket_page() #переход на страницу корзины
+    page.go_to_basket_page()
     basket_page = BasketPage(browser, link)
     basket_page.should_be_message_that_basket_empty()
     basket_page.basket_is_empty()
