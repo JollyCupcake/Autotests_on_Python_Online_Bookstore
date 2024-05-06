54f14c19c832a4b1995b6c2de837b359c713e321
JollyCupcake <60314990+JollyCupcake@users.noreply.github.com>
2024-05-06 17:44:05 +0400

    removed odd comments

--- test_main_page.py
+++ test_main_page.py
@@ -10,11 +10,11 @@ import pytest
 class TestLoginFromMainPage():
     def test_guest_can_go_to_login_page(self, browser):
         link = "http://selenium1py.pythonanywhere.com/"
-        page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
-        page.open()                      # открываем страницу
-        page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
-        login_page = LoginPage(browser, browser.current_url) # инициализируем Page Object LoginPage
-        login_page.should_be_login_page()  # вызываем метод - проверку наличия форм регистрации и логина, проверку урла
+        page = MainPage(browser, link)
+        page.open()
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
 
     def test_guest_should_see_login_link(self, browser):
         link = "http://selenium1py.pythonanywhere.com/"
@@ -26,7 +26,7 @@ def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
-    page.go_to_basket_page() #переход на страницу корзины
+    page.go_to_basket_page()
     basket_page = BasketPage(browser, link)
     basket_page.should_be_message_that_basket_empty()
     basket_page.basket_is_empty()
